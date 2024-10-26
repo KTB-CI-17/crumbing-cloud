78e8c33bbcecd8c6ece51ae1a793449d2af2c9ff
goorm <goorm@goorms-MacBook-Pro.local>
2024-10-26 22:14:27 +0900

    ai, cloud 서브넷에 적용할 라우팅 테이블 추가

--- terraform-C/modules/route_table/variable.tf
+++ terraform-C/modules/route_table/variable.tf
@@ -28,6 +28,17 @@ variable "subnet_be_id" {
   type        = string
 }
 
+variable "subnet_ai_id" {
+  description = "AI 서브넷의 ID"
+  type        = string
+}
+
+variable "subnet_cloud_id" {
+  description = "클라우드 서브넷의 ID"
+  type        = string
+}
+
+
 variable "route_table_private_name" {
   description = "프라이빗 라우트 테이블 이름"
   type        = string
