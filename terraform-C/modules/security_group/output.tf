f4181a724174f3a54320592d1c866aadda013c35
goorm <goorm@goorms-MacBook-Pro.local>
2024-10-26 22:15:13 +0900

    feat: ai,cloud 인스턴스의 보안 그룹 추가

--- terraform-C/modules/security_group/output.tf
+++ terraform-C/modules/security_group/output.tf
@@ -7,3 +7,13 @@ output "sg_be_id" {
   description = "백 보안 그룹의 ID"
   value       = aws_security_group.modules_sg_be.id
 }
+
+output "sg_ai_id" {
+  description = "ai 보안 그룹 ID"
+  value       = aws_security_group.modules_sg_ai.id
+}
+
+output "sg_cloud_id" {
+  description = "클라우드 보안 그룹 ID"
+  value       = aws_security_group.modules_sg_cloud.id
+}
