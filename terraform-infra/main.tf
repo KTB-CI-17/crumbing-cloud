1d3637eda422ac10565274e3d913d0b6c74224a8
ChoiseU <csw980928@gmail.com>
2024-11-23 23:53:20 +0900

    refactor: ai 인스턴스 주석 및 쉘 스크립트 수정

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -109,13 +109,13 @@ module "instance" {
       volume_size        = var.volume_size
       volume_type        = var.volume_type
     }
-    worker-ai = {
-      subnet_id           = module.subnet.private_subnet_ids[0]
-      security_group_ids  = [module.security_group.sg_worker_id]
-      instance_name       = var.instance_worker_ai_name
-      volume_size        = var.volume_size
-      volume_type        = var.volume_type
-    }
+#    worker-ai = {
+#      subnet_id           = module.subnet.private_subnet_ids[0]
+#      security_group_ids  = [module.security_group.sg_worker_id]
+#      instance_name       = var.instance_worker_ai_name
+#      volume_size        = var.volume_size
+#      volume_type        = var.volume_type
+#    }
   }
 }
 
