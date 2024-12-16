90015104db19d463b6dd70cfe78742da4b2d674c
ChoiseU <csw980928@gmail.com>
2024-12-16 20:13:17 +0900

    refactor: 보안그룹 열어두기

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -112,6 +112,13 @@ module "instance" {
       volume_size        = var.volume_size
       volume_type        = var.volume_type
     }
+    worker-ai = {
+      subnet_id           = module.subnet.private_subnet_ids[1]
+      security_group_ids  = [module.security_group.sg_worker_id]
+      instance_name       = var.instance_worker_ai_name
+      volume_size        = var.volume_size
+      volume_type        = var.volume_type
+    }
     nfs = {
       subnet_id          = module.subnet.private_subnet_ids[1]
       security_group_ids = [module.security_group.sg_nfs_id]
@@ -119,13 +126,6 @@ module "instance" {
       volume_size        = var.volume_size
       volume_type        = var.volume_type
     }
-#    worker-ai = {
-#      subnet_id           = module.subnet.private_subnet_ids[0]
-#      security_group_ids  = [module.security_group.sg_worker_id]
-#      instance_name       = var.instance_worker_ai_name
-#      volume_size        = var.volume_size
-#      volume_type        = var.volume_type
-#    }
   }
 }
 
